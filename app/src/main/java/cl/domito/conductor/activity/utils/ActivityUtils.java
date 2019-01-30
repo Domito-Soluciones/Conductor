d7bbd93208d428663c5073aae313d285446fa88d
Jose Sanchez <jsanchez@adportas.cl>
2019-01-30 20:25:18 -0300

    ghebvui eb

--- app/src/main/java/cl/domito/conductor/activity/utils/ActivityUtils.java
+++ app/src/main/java/cl/domito/conductor/activity/utils/ActivityUtils.java
@@ -209,7 +209,8 @@ public class ActivityUtils {
                 e.printStackTrace();
             }
         }
-        LatLngBounds.Builder builder = new LatLngBounds.Builder(); builder.include(latLngInicio).include(latLngFin);
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        builder.include(latLngInicio).include(latLngFin);
         activity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
